7d064e5ea97dd2baa711ea3a2779f33e60fe52b9
Daniel Phillimore <dan@ovms.co>
2015-08-02 03:13:42 +0100

    Added test for transpilation of PHP print expressions

---
+++ test/integration/transpiler/printTest.js
@@ -0,0 +1,40 @@
+/*
+ * PHPToJS - PHP-to-JavaScript transpiler
+ * Copyright (c) Dan Phillimore (asmblah)
+ * https://github.com/uniter/phptojs
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptojs/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    phpToJS = require('../../..');
+
+describe('Transpiler "print" expression test', function () {
+    it('should correctly transpile in default (async) mode', function () {
+        var ast = {
+            name: 'N_PROGRAM',
+            statements: [{
+                name: 'N_EXPRESSION_STATEMENT',
+                expression: {
+                    name: 'N_PRINT_EXPRESSION',
+                    operand: {
+                        name: 'N_STRING',
+                        string: 'hello'
+                    }
+                }
+            }]
+        };
+
+        expect(phpToJS.transpile(ast)).to.equal(
+            'require(\'phpruntime\')(function (stdin, stdout, stderr, tools, namespace) {' +
+            'var namespaceScope = tools.createNamespaceScope(namespace), namespaceResult, scope = tools.globalScope, currentClass = null;' +
+            '(stdout.write(namespaceScope.getConstant("hello").coerceToString().getNative()), ' +
+            'tools.valueFactory.createInteger(1));' +
+            'return tools.valueFactory.createNull();' +
+            '});'
+        );
+    });
+});
