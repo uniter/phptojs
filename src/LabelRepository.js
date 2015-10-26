5aa80ba6a34d15a7220ceb157dfdec832075adef
Daniel Phillimore <dan@ovms.co>
2015-10-26 23:35:17 +0000

    Replaced Lodash with Microdash

--- src/LabelRepository.js
+++ src/LabelRepository.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-var _ = require('lodash'),
+var _ = require('microdash'),
     util = require('util'),
     EventEmitter = require('events').EventEmitter;
 
--- src/transpilerSpec.js
+++ src/transpilerSpec.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-var _ = require('lodash'),
+var _ = require('microdash'),
     BARE = 'bare',
     RUNTIME_PATH = 'runtimePath',
     SYNC = 'sync',
