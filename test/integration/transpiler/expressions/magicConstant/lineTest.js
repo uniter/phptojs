1ff65059d06e92b16fe4c4d1e6451cb77b856cbe
Daniel Phillimore <dan@ovms.co>
2015-11-19 18:46:28 +0000

    Add tests for existing __DIR__, __FILE__ and __LINE__ support

---
+++ test/integration/transpiler/expressions/magicConstant/lineTest.js
@@ -0,0 +1,77 @@
+/*
+ * PHPToJS - PHP-to-JavaScript transpiler
+ * Copyright (c) Dan Phillimore (asmblah)
+ * https://github.com/uniter/phptojs
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptojs/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    phpToJS = require('../../../../..');
+
+describe('Transpiler __LINE__ magic constant test', function () {
+    it('should correctly transpile a return statement outside of function', function () {
+        var ast = {
+            name: 'N_PROGRAM',
+            statements: [{
+                name: 'N_RETURN_STATEMENT',
+                expression: {
+                    name: 'N_MAGIC_LINE_CONSTANT',
+                    offset: {
+                        line: 1,
+                        offset: 2
+                    }
+                }
+            }]
+        };
+
+        expect(phpToJS.transpile(ast, {bare: true})).to.equal(
+            'function (stdin, stdout, stderr, tools, namespace) {' +
+            'var namespaceScope = tools.createNamespaceScope(namespace), namespaceResult, scope = tools.globalScope, currentClass = null;' +
+            'return tools.valueFactory.createInteger(1);' +
+            'return tools.valueFactory.createNull();' +
+            '}'
+        );
+    });
+
+    it('should correctly transpile a return statement inside function', function () {
+        var ast = {
+            name: 'N_PROGRAM',
+            statements: [
+                {
+                    'name': 'N_FUNCTION_STATEMENT',
+                    'func': 'myFunction',
+                    'args': [],
+                    'body': {
+                        'name': 'N_COMPOUND_STATEMENT',
+                        'statements': [
+                            {
+                                'name': 'N_RETURN_STATEMENT',
+                                'expression': {
+                                    'name': 'N_MAGIC_LINE_CONSTANT',
+                                    'offset': {
+                                        'line': 3,
+                                        'offset': 8
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        };
+
+        expect(phpToJS.transpile(ast, {bare: true})).to.equal(
+            'function (stdin, stdout, stderr, tools, namespace) {' +
+            'var namespaceScope = tools.createNamespaceScope(namespace), namespaceResult, scope = tools.globalScope, currentClass = null;' +
+            'namespace.defineFunction("myFunction", function () {var scope = this;' +
+            'return tools.valueFactory.createInteger(3);' +
+            '});' +
+            'return tools.valueFactory.createNull();' +
+            '}'
+        );
+    });
+});
