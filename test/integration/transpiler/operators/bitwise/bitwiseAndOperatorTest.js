d324c4007cccb53c2423e0c77960b3085c136fa9
Daniel Phillimore <dan@ovms.co>
2015-12-03 22:32:47 +0000

    Add support for binary bitwise operators

---
+++ test/integration/transpiler/operators/bitwise/bitwiseAndOperatorTest.js
@@ -0,0 +1,46 @@
+/*
+ * PHPToJS - PHP-to-JavaScript transpiler
+ * Copyright (c) Dan Phillimore (asmblah)
+ * https://github.com/uniter/phptojs
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptojs/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    phpToJS = require('../../../../..');
+
+describe('Transpiler bitwise AND operator "&" test', function () {
+    it('should correctly transpile a return statement with operation', function () {
+        var ast = {
+            name: 'N_PROGRAM',
+            statements: [{
+                name: 'N_RETURN_STATEMENT',
+                expression: {
+                    name: 'N_EXPRESSION',
+                    left: {
+                        name: 'N_INTEGER',
+                        number: '21'
+                    },
+                    right: [{
+                        operator: '&',
+                        operand: {
+                            name: 'N_INTEGER',
+                            number: '10'
+                        }
+                    }]
+                }
+            }]
+        };
+
+        expect(phpToJS.transpile(ast)).to.equal(
+            'require(\'phpruntime\').compile(function (stdin, stdout, stderr, tools, namespace) {' +
+            'var namespaceScope = tools.createNamespaceScope(namespace), namespaceResult, scope = tools.globalScope, currentClass = null;' +
+            'return tools.valueFactory.createInteger(21).bitwiseAnd(tools.valueFactory.createInteger(10));' +
+            'return tools.valueFactory.createNull();' +
+            '});'
+        );
+    });
+});
