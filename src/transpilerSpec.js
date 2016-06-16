aee5ff8b7b8d851f8d4d7101cb2a7094592fc70c
Daniel Phillimore <dan@ovms.co>
2016-06-16 22:12:46 +0100

    Fix error
    - .isTheClassOfObject(...) requires `namespaceScope`

--- src/transpilerSpec.js
+++ src/transpilerSpec.js
@@ -988,7 +988,7 @@ module.exports = {
                 code = '';
 
             _.each(node.catches, function (catchNode, index) {
-                var catchCode = 'if (' + interpret(catchNode.type, {allowBareword: true}) + '.isTheClassOfObject(e)) {' +
+                var catchCode = 'if (' + interpret(catchNode.type, {allowBareword: true}) + '.isTheClassOfObject(e, namespaceScope)) {' +
                     interpret(catchNode.variable, {getValue: false}) + '.setValue(e);' +
                     interpret(catchNode.body) +
                     '}';
