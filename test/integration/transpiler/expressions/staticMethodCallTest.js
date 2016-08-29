52c45f3082deac7449fd35eec798868a0c5b0c44
Daniel Phillimore <dan@ovms.co>
2016-08-29 22:52:20 +0100

    Add test for static method call handling

---
+++ test/integration/transpiler/expressions/staticMethodCallTest.js
@@ -0,0 +1,44 @@
+/*
+ * PHPToJS - PHP-to-JavaScript transpiler
+ * Copyright (c) Dan Phillimore (asmblah)
+ * https://github.com/uniter/phptojs
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptojs/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    phpToJS = require('../../../..');
+
+describe('Transpiler static method call expression test', function () {
+    it('should correctly transpile a call to static method with FQCN', function () {
+        var ast = {
+            name: 'N_PROGRAM',
+            statements: [{
+                name: 'N_EXPRESSION_STATEMENT',
+                expression: {
+                    name: 'N_STATIC_METHOD_CALL',
+                    className: {
+                        name: 'N_STRING',
+                        string: '\\My\\Space\\MyClass'
+                    },
+                    method: {
+                        name: 'N_STRING',
+                        string: 'myMethod'
+                    },
+                    args: []
+                }
+            }]
+        };
+
+        expect(phpToJS.transpile(ast)).to.equal(
+            'require(\'phpruntime\').compile(function (stdin, stdout, stderr, tools, namespace) {' +
+            'var namespaceScope = tools.createNamespaceScope(namespace), namespaceResult, scope = tools.topLevelScope, currentClass = null;' +
+            'tools.valueFactory.createBarewordString("\\\\My\\\\Space\\\\MyClass").callStaticMethod(tools.valueFactory.createBarewordString("myMethod"), [], namespaceScope);' +
+            'return tools.valueFactory.createNull();' +
+            '});'
+        );
+    });
+});
