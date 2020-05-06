c32c0183ddf3138c6fc957cafa4318498768cbb6
Daniel Phillimore <dan@ovms.co>
2020-05-06 19:34:04 +0100

    Add support for transpiling PHP "clone" operator

---
+++ test/integration/transpiler/operators/cloneTest.js
@@ -0,0 +1,39 @@
+/*
+ * PHPToJS - PHP-to-JavaScript transpiler
+ * Copyright (c) Dan Phillimore (asmblah)
+ * https://github.com/uniter/phptojs
+ *
+ * Released under the MIT license
+ * https://github.com/uniter/phptojs/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    phpToJS = require('../../../..');
+
+describe('Transpiler clone operator test', function () {
+    it('should correctly transpile a return of a cloned variable', function () {
+        var ast = {
+            name: 'N_PROGRAM',
+            statements: [{
+                name: 'N_RETURN_STATEMENT',
+                expression: {
+                    name: 'N_CLONE_EXPRESSION',
+                    operand: {
+                        name: 'N_VARIABLE',
+                        variable: 'myObject'
+                    }
+                }
+            }]
+        };
+
+        expect(phpToJS.transpile(ast)).to.equal(
+            'require(\'phpruntime\').compile(function (stdin, stdout, stderr, tools, namespace) {' +
+            'var namespaceScope = tools.topLevelNamespaceScope, namespaceResult, scope = tools.topLevelScope, currentClass = null;' +
+            'return scope.getVariable("myObject").getValue().clone();' +
+            'return tools.valueFactory.createNull();' +
+            '});'
+        );
+    });
+});
